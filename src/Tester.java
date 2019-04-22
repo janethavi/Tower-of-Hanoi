151cb74df67c8233ac5fc38ac79fbf864f8f4fdf
janethavi <janethavishka@gmail.com>
2019-04-22 16:02:27 +0530

    java project on tower of hanoi

---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args) {
+        int n = 4; // Number of disks
+        //Here we have three rods. A, B and C are names of rods
+        TowerOfHanoi.towerOfHanoi(n, 'A', 'C', 'B');
+    }
+}
